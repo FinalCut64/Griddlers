0b3d54e5a389db0d7bab39cf76df307dd2a91537
FinalCut64 <42783212+FinalCut64@users.noreply.github.com>
2018-10-19 20:25:08 -0300

    agregado this.mayor y this.mayorindice en Linea

--- linea.js
+++ linea.js
@@ -4,22 +4,29 @@ class Linea {
     this.tipo = tipo;
     this.colrow = colrow;
     this.cant = cant;
-    this.iniciosn = []; //referidos a islas negras
-    this.finesn = [];
-    this.tamañosn = [];
+    this.inicios = []; //referidos a islas negras
+    this.fines = [];
+    this.tamaños = [];
+    this.mayor = -1; //inicio con un valor imposible
+    this.mayorindice = -1;
 
     // this.iniciosb = []; //referidos a islas blancas
     // this.finesb = [];
     // this.tamañosb = [];
   }
 
-  //pruebita
-
   completar() {
     let aux = inifintam(this.tipo, this.colrow, 1); //islas negras
     this.inicios = aux.ini;
     this.fines = aux.fin;
     this.tamaños = aux.tam;
+
+    for (let i = 0; i < this.tamaños.length; i++) {
+      if (this.tamaños[i] > this.mayor) {
+        this.mayor = this.tamaños[i];
+        this.mayorindice = i;
+      }
+    }
 
     // aux = inifintam(this.tipo, this.colrow, 2); //islas blancas
     // this.inicios = aux.ini;
